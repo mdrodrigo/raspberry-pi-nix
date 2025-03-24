1b966debdd679fae8ae95cc28993d03e78dd6dca
Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>
2025-03-24 10:51:32 -0300

    rpi/default: Enable u-boot like bootloader
    Signed-off-by: Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>

--- rpi/default.nix
+++ rpi/default.nix
@@ -61,7 +61,7 @@ in
       };
       uboot = {
         enable = mkOption {
-          default = false;
+          default = true;
           type = types.bool;
           description = ''
             If enabled then uboot is used as the bootloader. If disabled
